2223215478030f2d2a6dc6689339cd90cc78e098
Ellon <mordecaimwema11@gmail.com>
2022-05-23 21:37:53 +0300

    paginations

--- isr/feed.js
+++ isr/feed.js
@@ -1,6 +1,3 @@
-// in progress
-
-
 const { IgApiClient } = require('instagram-private-api');
 const winston = require('winston');
 const download = require('download');
@@ -22,40 +19,110 @@ const logger = winston.createLogger(logConfig);
     const auth = await ig.account.login(Config.Instagram.Username, Config.Instagram.Password);
     const postsFeed = ig.feed.user(await ig.user.getIdByUsername(Config.FeedTarget.target));
     const wholeRes = await postsFeed.request();
+
+    const folderPath = path.join(
+      Config.FeedFolder,
+      Config.FeedTarget.target,   
+    )
     
     // paginated results
     const items = await postsFeed.items();
-    const items2 = await postsFeed.items();
-    const items3 = await postsFeed.items();
-    const items4 = await postsFeed.items();
-    //....
- 
-    const folderPath = path.join(
-        Config.FeedFolder,
-        Config.FeedTarget.target,   
-    )
     
-    // wrap in fn
+    logger.log({
+      message: 'Carousel: first batch starting..',
+      level: 'info'
+    })
+    items.forEach((item) => {
+          if (item.carousel_media_count > 0){
+              item.carousel_media.forEach((media) => {
+               media.image_versions2.candidates.forEach((candidate) => {
+                  download(candidate.url, folderPath);
+               })
+               if (media.video_versions != undefined) {
+                media.video_versions.forEach((candidate) => {
+                  download(candidate.url, folderPath);
+               })
+               }
+              })
+          }
+      })
 
-    // carousel images
-    console.log('dl started:..');
-    // items3.forEach((item) => {
-    //     if (item.carousel_media_count > 0){
-    //         item.carousel_media.forEach((media) => {
-    //          media.image_versions2.candidates.forEach((candidate) => {
-    //             download(candidate.url, folderPath);
-    //          })
-    //         })
-    //     }
-    // })
+    // second batch
+    const items2 = await postsFeed.items();
+    logger.log({
+      message: 'Carousel: second batch starting..',
+      level: 'info'
+    })
+    setTimeout(() => {
+    items2.forEach((item) => {
+      if (item.carousel_media_count > 0){
+          item.carousel_media.forEach((media) => {
+           media.image_versions2.candidates.forEach((candidate) => {
+              download(candidate.url, folderPath);
+           })
+           if (media.video_versions != undefined) {
+            media.video_versions.forEach((candidate) => {
+              download(candidate.url, folderPath);
+           })
+           }
+          })
+      }
+    })
+  }, 6000);
 
-    //single images 
+    // third batch 
+    const items3 = await postsFeed.items();
+    logger.log({
+      message: 'Carousel: third batch starting..',
+      level: 'info'
+    })
+    setTimeout(() => {
+    items3.forEach((item) => {
+      if (item.carousel_media_count > 0){
+          item.carousel_media.forEach((media) => {
+           media.image_versions2.candidates.forEach((candidate) => {
+              download(candidate.url, folderPath);
+           })
+           if (media.video_versions != undefined) {
+            media.video_versions.forEach((candidate) => {
+              download(candidate.url, folderPath);
+           })
+           }
+          })
+      }
+    })
+  }, 12000);
 
-    //wrap in fn
+    // fourth batch
+    const items4 = await postsFeed.items();
+    logger.log({
+      message: 'Carousel: fourth batch starting..',
+      level: 'info'
+    })
+    setTimeout(() => {
+    items4.forEach((item) => {
+      if (item.carousel_media_count > 0){
+          item.carousel_media.forEach((media) => {
+           media.image_versions2.candidates.forEach((candidate) => {
+              download(candidate.url, folderPath);
+           })
+           if (media.video_versions != undefined) {
+            media.video_versions.forEach((candidate) => {
+              download(candidate.url, folderPath);
+           })
+           }
+          })
+      }
+    })
+  }, 18000);
+   
+  //....
+
+    //single images
 
     items.forEach((item) => {
         logger.log({
-            message: 'first batch starting..',
+            message: 'Single: first batch starting..',
             level: 'info'
         })
         if (item.image_versions2 != undefined){
@@ -63,11 +130,16 @@ const logger = winston.createLogger(logConfig);
             download(candidate.url, folderPath);
           })
         }
-    })
+        if (item.video_versions != undefined){
+          item.video_versions.forEach((candidate) => {
+            download(candidate.url, folderPath);
+          })
+        }
+    });
 
     setTimeout(() => {
         logger.log({
-            message: 'second batch starting..',
+            message: 'Single: second batch starting..',
             level: 'info'
         })
         items2.forEach((item) => {
@@ -76,12 +148,17 @@ const logger = winston.createLogger(logConfig);
                 download(candidate.url, folderPath);
               })
             }
-        })
-    }, 6000 )
+          if (item.video_versions != undefined){
+                item.video_versions.forEach((candidate) => {
+                  download(candidate.url, folderPath);
+                })
+              }
+              })
+      }, 6000);
 
     setTimeout(() => {
         logger.log({
-            message: 'third batch starting..',
+            message: 'Single: third batch starting..',
             level: 'info'
         })
         items3.forEach((item) => {
@@ -90,26 +167,35 @@ const logger = winston.createLogger(logConfig);
                 download(candidate.url, folderPath);
               })
             }
-        })
-    }, 12000)
-
-    setTimeout(() => {
-        logger.log({
-            message: 'fourth batch starting..',
-            level: 'info'
-        })
-        items4.forEach((item) => {
-            if (item.image_versions2 != undefined){
-              item.image_versions2.candidates.forEach((candidate) => {
+             if (item.video_versions != undefined){
+              item.video_versions.forEach((candidate) => {
                 download(candidate.url, folderPath);
               })
             }
         })
-    }, 18000)
+    }, 12000);
 
-    logger.log({
-        message: `Finito, ${Config.FeedTarget.target}`,
-        level: 'info'
-    })
+    setTimeout(() => {
+        logger.log({
+            message: 'Single: fourth batch starting..',
+            level: 'info'
+        })
+        items4.forEach((item) => {
+            if (item.image_versions2 != undefined) {
+              item.image_versions2.candidates.forEach((candidate) => {
+                download(candidate.url, folderPath);
+              })
+            }
+             if (item.video_versions != undefined){
+              item.video_versions.forEach((candidate) => {
+              download(candidate.url, folderPath);
+          })
+        }
+        })
 
+        logger.log({
+          message: `Finito, ${Config.FeedTarget.target}`,
+          level: 'info'
+      })
+    }, 18000);
 })();
